8645c1a0775284875a27543c92dcf4222f111128
antriksh mahajan <antrikshmahajan5@gmail.com>
2025-03-14 13:27:04 +0530

    draft frontend render done

--- src/api/admin/orders/route.ts
+++ src/api/admin/orders/route.ts
@@ -2,12 +2,11 @@ import type { MedusaRequest, MedusaResponse } from "@medusajs/framework/http";
 import { getOrdersListWorkflow } from "@medusajs/medusa/core-flows";
 
 export async function GET(req: MedusaRequest, res: MedusaResponse) {
-  console.log(req.queryConfig.fields);
   const { result } = await getOrdersListWorkflow(req.scope).run({
     input: {
       fields: req.queryConfig.fields,
     },
   });
-  console.log(`@@@@@@@@@@@@@@@@`);
-  res.send(result);
+
+  res.send({ orders: result, count: 25, limit: 20, offset: 0 });
 }
